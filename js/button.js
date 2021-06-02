a1ac0555fbe74a8f74a13c098e24ed200bf88d97
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-02 22:35:38 +0800

    refactor(): cleanup some code

--- js/button.js
+++ js/button.js
@@ -1,33 +1,32 @@
-this.$ = this.jQuery = jQuery.noConflict(true); $("iframe[name='result']").each(function() { this.sandbox += ' allow-modals'; });
 $(document).ready(function () {
-	res = 0;
-	$(".noselect").on("click", function (event) {
-		
-		if ($(event.target).is('.noselect')) {
-			$('.select').addClass('noselect');
-			$('.select').removeClass('select')
-			$(event.target).addClass('select');
-			$(event.target).removeClass('noselect')
-			
-			for(i = 1; i <= 4; i++){
-				if($(event.target).is('#' + i)) res = i; 
-			}
+  let response = 0;
 
-		}
-	});
-	
-	$("#button").on("click", function () {
-		if(res == 0){
-			swal("", "你還未選取答案，請先選取答案", "error");
-			//alert("你還未選取答案，請先選取答案")
-		}
-		else if(res == 1 || res == 2 || res == 3){
-			swal("", "答案錯了喔", "error");
-			//alert("答案錯了喔")
-		}
-		else if(res == 4){
-			swal("", "正確答案", "success");
-			//alert("正確答案")
-		}
-	});
+  $(".not-selected").on("click", function (event) {
+    if ($(event.target).is(".not-selected")) {
+      $(".selected").addClass("not-selected");
+      $(".selected").removeClass("selected");
+      $(event.target).addClass("selected");
+      $(event.target).removeClass("not-selected");
+
+      for (let i = 1; i <= 4; i++) {
+        if ($(event.target).is(`#select-${i}`)) response = i;
+      }
+    }
+  });
+
+  $("#button").on("click", function () {
+    switch (response) {
+      case 0:
+        swal("", "你還未選取答案，請先選取答案", "error");
+        break;
+      case 1:
+      case 2:
+      case 3:
+        swal("", "答案錯了喔", "error");
+        break;
+      case 4:
+        swal("", "正確答案", "success");
+        break;
+    }
+  });
 });
