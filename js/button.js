dfbd44fab2ddcf8d64363cfd25b8c5dc9a9a8d35
yuspring <75129303+yuspring@users.noreply.github.com>
2021-06-03 01:05:37 +0800

    Update button.js

--- js/button.js
+++ js/button.js
@@ -1,7 +1,9 @@
+
 $(document).ready(function () {
-  let response = 0;
+  let res = 0;
 
   $(".not-selected").on("click", function (event) {
+
     if ($(event.target).is(".not-selected")) {
       $(".selected").addClass("not-selected");
       $(".selected").removeClass("selected");
@@ -9,24 +11,38 @@ $(document).ready(function () {
       $(event.target).removeClass("not-selected");
 
       for (let i = 1; i <= 4; i++) {
-        if ($(event.target).is(`#select-${i}`)) response = i;
+        if ($(event.target).is(`#select-${i}`)) res = i;
       }
     }
-  });
+
+  })
 
   $(".button").on("click", function () {
-    switch (response) {
-      case 0:
-        swal("", "你還未選取答案，請先選取答案", "error");
-        break;
-      case 1:
-      case 2:
-      case 3:
-        swal("", "答案錯了喔", "error");
-        break;
-      case 4:
-        swal("", "正確答案", "success");
-        break;
+
+    if (res == 0) {
+      swal.fire({
+        title: '你還未選取答案，請先選取答案',
+        icon: 'error',
+        background: '#ECF5FF',
+        backdrop: `rgba(255,255,255,0)`
+      })
     }
-  });
-});
+    else if (res >= 1 && res <= 3) {
+      swal.fire({
+        title: '答案錯了喔',
+        icon: 'error',
+        background: '#ECF5FF',
+        backdrop: `rgba(255,255,255,0)`
+      })
+    }
+    else if (res == 4) {
+      swal.fire({
+        title: '正確答案',
+        icon: 'success',
+        background: '#ECF5FF',
+        backdrop: `rgba(255,255,255,0)`
+      })
+    }
+  })
+
+})
